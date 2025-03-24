0a325902d0c94c209ccdb3650d6e36c44db065f6
kivinew <kivinew@gmail.com>
2025-03-24 23:47:39 +0700

    поиск по серийнику включает заглавные и строчные буквы

--- GPON_info_by_SN.py
+++ GPON_info_by_SN.py
@@ -9,14 +9,14 @@ crt.Screen.Synchronous = True
 
 def main():
 	
-	memBuffer = pyperclip.paste()
+	mem_buffer = pyperclip.paste()
 
 	# Проверка на соответствие шаблону серийных номеров HUAWEI
-	if not (memBuffer.startswith('48575443') or memBuffer.startswith('HWTC')):
+	if not (mem_buffer.startswith('48575443') or mem_buffer.upper().startswith('HWTC')):
 		crt.Screen.Send("display ont info by-sn ")
 		return
 
-	crt.Screen.Send(f"display ont info by-sn {memBuffer}\r")
+	crt.Screen.Send(f"display ont info by-sn {mem_buffer.upper()}\r")
 	if (crt.Screen.WaitForString("---- More ( Press 'Q' to break ) ----", 2) == 1):
 		crt.Screen.Send("q")
 
